f35fad3086463419e1438438c856a1c1ee968276
Blake Smith <blakeinvictoria@gmail.com>
2020-11-23 13:44:34 -0800

    Add workflow skeleton and unit tests.

---
+++ tests/test_workflow.py
@@ -0,0 +1,30 @@
+def test_map_default_isolates():
+    assert False
+
+
+def test_generate_isolate_fasta():
+    assert False
+
+
+def test_build_isolate_index():
+    assert False
+
+
+def test_map_isolates():
+    assert False
+
+
+def test_map_subtraction():
+    assert False
+
+
+def test_subtract_mapping():
+    assert False
+
+
+def test_pathoscope():
+    assert False
+
+
+def test_workflow():
+    assert False
---
+++ workflow.py
@@ -0,0 +1,40 @@
+from virtool_workflow import step
+
+
+@step
+def map_default_isolates():
+    # TODO:
+    ...
+
+
+@step
+def generate_isolate_fasta():
+    # TODO:
+    ...
+
+
+@step
+def build_isolate_index():
+    ...
+
+
+@step
+def map_isolates():
+    ...
+
+
+@step
+def map_subtraction():
+    ...
+
+
+@step
+def subtract_mapping():
+    ...
+
+
+@step
+def pathoscope():
+    ...
+
+
