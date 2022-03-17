27a27a446770289b5097a2eea8cac6051fcb1d86
Ian Boyes <igboyes@gmail.com>
2022-03-16 17:32:24 -0700

    fix: avoid unnecessary file operations
    * Try to avoid writing and reading files as much as possible.
    * Remove unused or unnecessary test data.
    * Use syrupy for snapshots.
    * Use datafiles fixture where appropriate.
    * Remove unused fixtures.

--- tests/test_pathoscope.py
+++ tests/test_pathoscope.py
@@ -1,8 +1,5 @@
-import filecmp
 import pickle
-import shutil
 from pathlib import Path
-from typing import Dict, Any
 
 import pytest
 
@@ -16,12 +13,7 @@ SAM_PATH = BASE_PATH / "test_al.sam"
 SCORES = BASE_PATH / "scores"
 TSV_PATH = BASE_PATH / "report.tsv"
 UNU_PATH = BASE_PATH / "unu"
-UPDATED_VTA_PATH = BASE_PATH / "updated.vta"
 VTA_PATH = BASE_PATH / "test.vta"
-
-
-def convert_int_keys_to_str(str_keyed_dict: Dict[int, Any]) -> Dict[str, Any]:
-    return {str(key): value for key, value in str_keyed_dict.items()}
 
 
 @pytest.fixture(scope="session")
@@ -41,12 +33,14 @@ def expected_scores():
 
 
 def test_find_sam_align_score(sam_line, expected_scores):
-    assert pathoscope.find_sam_align_score(sam_line) == expected_scores["".join(sam_line)]
+    assert (
+        pathoscope.find_sam_align_score(sam_line) == expected_scores["".join(sam_line)]
+    )
 
 
-def test_build_matrix(data_regression, tmp_path):
-    shutil.copy(VTA_PATH, tmp_path)
-    vta_path = tmp_path / "test.vta"
+@pytest.mark.datafiles(VTA_PATH)
+def test_build_matrix(data_regression, datafiles):
+    vta_path = datafiles / "test.vta"
 
     with open(MATRIX_PATH, "rb") as handle:
         expected = pickle.load(handle)
@@ -54,12 +48,7 @@ def test_build_matrix(data_regression, tmp_path):
     actual = pathoscope.build_matrix(vta_path, 0.01)
     u, nu, refs, reads = actual
 
-    data_regression.check([
-        u,
-        nu,
-        sorted(refs),
-        sorted(reads)
-    ])
+    data_regression.check([u, nu, sorted(refs), sorted(reads)])
 
     assert expected[0] == actual[0]
     assert expected[1] == actual[1]
@@ -67,32 +56,38 @@ def test_build_matrix(data_regression, tmp_path):
     assert sorted(expected[3]) == sorted(actual[3])
 
 
+@pytest.mark.datafiles(VTA_PATH)
 @pytest.mark.parametrize("theta_prior", [0, 1e-5])
 @pytest.mark.parametrize("pi_prior", [0, 1e-5])
 @pytest.mark.parametrize("epsilon", [1e-6, 1e-7, 1e-8])
 @pytest.mark.parametrize("max_iter", [5, 10, 20, 30])
-def test_em(data_regression, tmp_path, theta_prior, pi_prior, epsilon, max_iter, expected_em):
-    shutil.copy(VTA_PATH, tmp_path)
-    vta_path = tmp_path / "test.vta"
+def test_em(
+    data_regression,
+    datafiles,
+    tmp_path,
+    theta_prior,
+    pi_prior,
+    epsilon,
+    max_iter,
+    expected_em,
+):
+    vta_path = datafiles / "test.vta"
 
     u, nu, refs, _ = pathoscope.build_matrix(vta_path, 0.01)
 
     result = pathoscope.em(u, nu, refs, max_iter, epsilon, pi_prior, theta_prior)
     init_pi, pi, theta, updated_nu = result
 
-    file_string = "_".join([str(i) for i in ["em", theta_prior, pi_prior, epsilon, max_iter]])
+    file_string = "_".join(
+        [str(i) for i in ["em", theta_prior, pi_prior, epsilon, max_iter]]
+    )
 
     for i in [0, 1, 2]:
         assert sorted(result[i]) == sorted(expected_em[file_string][i])
 
     assert result[3] == expected_em[file_string][3]
 
-    data_regression.check([
-        sorted(init_pi),
-        sorted(pi),
-        sorted(theta),
-        updated_nu
-    ])
+    data_regression.check([sorted(init_pi), sorted(pi), sorted(theta), updated_nu])
 
 
 def test_compute_best_hit(data_regression):
@@ -106,12 +101,12 @@ def test_compute_best_hit(data_regression):
     data_regression.check(pathoscope.compute_best_hit(*matrix_tuple))
 
 
-def test_rewrite_align(file_regression, tmp_path):
+@pytest.mark.datafiles(VTA_PATH)
+def test_rewrite_align(datafiles, file_regression, tmp_path):
     with open(UNU_PATH, "rb") as f:
         u, nu = pickle.load(f)
 
-    shutil.copy(VTA_PATH, tmp_path)
-    vta_path = tmp_path / "test.vta"
+    vta_path = datafiles / "test.vta"
 
     rewrite_path = tmp_path / "rewrite.vta"
 
@@ -120,15 +115,12 @@ def test_rewrite_align(file_regression, tmp_path):
     with open(rewrite_path, "r") as f:
         file_regression.check(f.read())
 
-    assert filecmp.cmp(UPDATED_VTA_PATH, rewrite_path)
-    assert not filecmp.cmp(vta_path, rewrite_path)
 
-
-def test_calculate_coverage(tmp_path, sam_path):
+@pytest.mark.datafiles(VTA_PATH)
+def test_calculate_coverage(datafiles, tmp_path, sam_path):
     ref_lengths = dict()
 
-    shutil.copy(VTA_PATH, tmp_path)
-    vta_path = tmp_path / "test.vta"
+    vta_path = datafiles / "test.vta"
 
     with open(sam_path, "r") as handle:
         for line in handle:
@@ -150,23 +142,28 @@ def test_calculate_coverage(tmp_path, sam_path):
     pathoscope.calculate_coverage(vta_path, ref_lengths)
 
 
-def test_write_report(file_regression, tmp_path):
-    shutil.copy(VTA_PATH, tmp_path)
-    vta_path = tmp_path / "test.vta"
+@pytest.mark.datafiles(VTA_PATH)
+def test_write_report(datafiles, file_regression, tmp_path):
+    vta_path = datafiles / "test.vta"
 
     u, nu, refs, reads = pathoscope.build_matrix(vta_path, 0.01)
 
     with open(BEST_HIT_PATH, "rb") as handle:
-        best_hit_initial_reads, best_hit_initial, level_1_initial, level_2_initial = pickle.load(handle)
+        (
+            best_hit_initial_reads,
+            best_hit_initial,
+            level_1_initial,
+            level_2_initial,
+        ) = pickle.load(handle)
 
     init_pi, pi, _, nu = pathoscope.em(u, nu, refs, 30, 1e-7, 0, 0)
 
-    best_hit_final_reads, best_hit_final, level_1_final, level_2_final = pathoscope.compute_best_hit(
-        u,
-        nu,
-        refs,
-        reads
-    )
+    (
+        best_hit_final_reads,
+        best_hit_final,
+        level_1_final,
+        level_2_final,
+    ) = pathoscope.compute_best_hit(u, nu, refs, reads)
 
     report_path = tmp_path / "report.tsv"
 
@@ -183,7 +180,7 @@ def test_write_report(file_regression, tmp_path):
         level_1_initial,
         level_2_initial,
         level_1_final,
-        level_2_final
+        level_2_final,
     )
 
     with open(report_path, "r") as f:
