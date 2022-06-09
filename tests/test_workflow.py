f7a10de13fd790e02e3faedc23be377069f7c94d
Ian Boyes <igboyes@gmail.com>
2022-06-09 08:14:30 -0700

    perf: use rust to speed up eliminate subtraction
    * Use SAM. Get rid of VTA (TSV) format.
    * Eliminate subtraction-mapped reads in Rust instead of Python.
    
    BREAKING CHANGE: Results change slightly because
    subtraction is removes low-scored alignments instead
    of reads.

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -13,6 +13,7 @@ from virtool_workflow.analysis.reads import Reads
 from virtool_workflow.data_model import NucleotideComposition, Subtraction
 from virtool_workflow.data_model.samples import Sample
 
+from pathoscope import parse_sam
 from workflow import (
     eliminate_subtraction,
     map_default_isolates,
@@ -24,7 +25,7 @@ TEST_DATA_PATH = Path(__file__).parent / "test_files"
 FASTQ_PATH = TEST_DATA_PATH / "test.fq"
 INDEX_PATH = TEST_DATA_PATH / "index"
 SUBTRACTION_PATH = TEST_DATA_PATH / "subtraction"
-VTA_PATH = TEST_DATA_PATH / "test.vta"
+SAM_PATH = TEST_DATA_PATH / "test_al.sam"
 
 
 @pytest.fixture
@@ -106,7 +107,6 @@ def read_file_names(sample):
 @pytest.fixture
 def reads(work_path: Path):
     shutil.copyfile(FASTQ_PATH, work_path / "reads_1.fq.gz")
-
     return Reads(sample=sample, quality={}, path=work_path)
 
 
@@ -156,7 +156,7 @@ def ref_lengths():
 
 
 async def test_map_default_isolates(
-    data_regression, read_file_names, index: Index, run_subprocess, snapshot
+    read_file_names, index: Index, run_subprocess, snapshot
 ):
     intermediate = SimpleNamespace(to_otus=set())
 
@@ -182,85 +182,86 @@ async def test_map_isolates(
 
     intermediate = SimpleNamespace(isolate_high_scores={})
     isolate_fastq_path = work_path / "mapped.fq"
-    isolate_vta_path = work_path / "isolates.vta"
+    isolate_sam_path = work_path / "to_isolates.sam"
 
     await map_isolates(
         read_file_names,
         intermediate,
         isolate_fastq_path,
         work_path / "isolates",
-        isolate_vta_path,
+        isolate_sam_path,
         run_subprocess,
         1,
         0.01,
     )
 
-    with isolate_vta_path.open("r") as f:
+    with isolate_sam_path.open("r") as f:
         assert sorted(line.rstrip() for line in f) == snapshot
 
     assert intermediate.isolate_high_scores == snapshot
 
 
-@pytest.mark.datafiles(VTA_PATH)
-async def test_eliminate_subtraction(
-    datafiles, subtraction, work_path, run_in_executor, run_subprocess
-):
+@pytest.mark.datafiles(SAM_PATH, FASTQ_PATH)
+async def test_eliminate_subtraction(datafiles, subtraction, work_path, run_subprocess):
+    isolate_fastq_path = work_path / "test.fq"
+    isolate_sam_path = work_path / "test_al.sam"
+
+    subtracted_path = work_path / "subtracted.sam"
+
     results = {}
 
-    isolate_fastq_path = work_path / "mapped.fastq"
-
-    shutil.copyfile(FASTQ_PATH, isolate_fastq_path)
-
-    with open(TEST_DATA_PATH / "isolate_high_scores.json", "r") as f:
-        intermediate = SimpleNamespace(isolate_high_scores=json.load(f))
-
-    isolate_vta_path = datafiles / "test.vta"
-
     await eliminate_subtraction(
-        intermediate,
         isolate_fastq_path,
-        isolate_vta_path,
+        isolate_sam_path,
         2,
         results,
-        run_in_executor,
         run_subprocess,
         subtraction,
+        subtracted_path,
         work_path,
     )
 
+    assert not (work_path / "to_subtraction.sam").is_file()
+    assert (work_path / "subtracted.sam").is_file()
     assert results["subtracted_count"] == 4
-    assert (work_path / "test.vta").is_file()
-    assert not (work_path / "subtracted.vta").is_file()
 
 
 async def test_pathoscope(
-    mocker, index, ref_lengths, run_in_executor, snapshot, work_path: Path
+    mocker,
+    data_regression,
+    file_regression,
+    index,
+    ref_lengths,
+    run_in_executor,
+    snapshot,
+    work_path: Path,
 ):
-    isolate_vta_path = work_path / "to_isolates.vta"
-
-    intermediate = SimpleNamespace(lengths=ref_lengths)
-
-    results = dict()
-
-    shutil.copyfile(VTA_PATH, isolate_vta_path)
+    subtracted_path = work_path / "subtracted.sam"
+    shutil.copyfile(SAM_PATH, subtracted_path)
 
     analysis = mocker.Mock(spec=Analysis)
+    intermediate = SimpleNamespace(lengths=ref_lengths)
+    results = {}
 
     await reassignment(
         analysis,
+        index,
         intermediate,
+        0.01,
         results,
         run_in_executor,
-        isolate_vta_path,
-        index,
-        0.01,
+        subtracted_path,
         work_path,
     )
 
-    analysis.upload.assert_called()
-
-    with intermediate.reassigned_path.open("r") as f:
-        assert sorted(line.rstrip() for line in f) == snapshot
+    data_regression.check(
+        sorted(
+            [
+                (line.read_id, line.ref_id, line.score)
+                for line in parse_sam(intermediate.reassigned_path)
+            ]
+        )
+    )
 
     with intermediate.report_path.open("r") as f:
-        assert sorted(line.rstrip() for line in f) == snapshot
+        file_regression.check(f.read(), extension=".tsv")
