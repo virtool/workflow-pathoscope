c2e9bb4dfb0bcebf871b107926362c3ccda1f48a
SnickerDoodlePop <markus@swoveland.com>
2023-01-10 11:02:13 -0800

    feat(SAMParser): implimented find_sam_align_score; completed file parser

--- utils/expectation_maximization/src/lib.rs
+++ utils/expectation_maximization/src/lib.rs
@@ -1,125 +1,186 @@
 #![allow(nonstandard_style)]
 #![allow(unused)]
 
-use std::{io::{BufReader, BufRead}, fs::File};
+use std::{path::Path, collections::{hash_map, HashMap}};
 
-struct SamLine
+
+fn buildMatrix(SAMPath: &Path, pScoreCutoff: Option<f32>)
 {
-    line: String,
+    let mut u: HashMap<i32, (Vec<i32>, Vec<f32>, Vec<f32>, f32)> = HashMap::new();
+    let mut nu: HashMap<i32, ((i32, i32), (f32, f32), f32, f32)> = HashMap::new();
 
-    fields: Option<Vec<String>>,
+    let mut hReadId: HashMap<&str, i32> = HashMap::new();
+    let mut hRefId: HashMap<&str, i32> = HashMap::new();
 
-    read_id: Option<String>,
-    read_length: Option<u32>,
-    position: Option<u32>,
-    score: Option<f32>,
-    btwsFlg: Option<u32>,
-    unmapped: Option<bool>,
-    ref_id: Option<String>,
+    let mut refs: Vec<String> = Vec::new();
+    let mut reads: Vec<String> = Vec::new();
+
+    let mut refCount: i32 = 0;
+    let mut readCount: i32 = 0;
+
+    let mut maxScore: f32 = 0.0;
+    let mut min_score = 0;
+
+
+    //TODO
+        //USE FILE PARSER HERE
+        //ITERATE OVER ELEMENTS AND DO MATH THINGS
+        //CREATE RESCALE_SAMSCORE FUNCTION
+
+
 }
 
-impl SamLine
+
+
+///Wrapper for the parseSAM function, which serves as the entry point for the module.
+mod SAMParser
 {
-    ///Create a new SamLine object by consuming a String object
-    ///
-    ///Returns none if the provided String is to be ignored
-    pub fn new(newLine: String) -> Option<SamLine>
+    use std::{io::{BufReader, BufRead}, fs::File, fmt::Debug};
+
+    #[derive(Debug)]
+    pub struct SamLine
     {
-        if newLine.is_empty() || newLine.starts_with("#") || newLine.starts_with("@")
+        read_id: String,
+        read_length: usize,
+        position: u32,
+        score: Option<f32>,
+        btwsFlg: u32,
+        unmapped: bool,
+        ref_id:String,
+        SAMfields: Vec<String>
+    }
+
+
+    impl SamLine
+    {
+        ///Create a new SamLine object by consuming a String object
+        ///
+        ///Returns none if the provided String is to be ignored
+        pub fn new(newLine: String) -> Option<SamLine>
         {
-            return None;
+            if newLine.is_empty() || newLine.starts_with("#") || newLine.starts_with("@")
+            {
+                return None;
+            }
+
+            let fields = newLine.split("\t").collect::<Vec<&str>>();
+
+            let mut newSamLine = SamLine
+            {
+                read_id: String::from(*(fields.get(0).expect("error parsing read_id"))), 
+                read_length: fields.get(9).expect("error parsing length field").len(),
+                position: fields.get(3).expect("error reading position field").parse::<u32>().expect("error parsing position as u32"),
+                score: None, 
+                btwsFlg: fields.get(1).expect("error reading btwsFlg field").parse::<u32>().expect("error parsing btwsFlg as u32"), 
+                unmapped: ((fields.get(1).unwrap().parse::<u32>().unwrap()) & (4 as u32) == (4 as u32)),
+                ref_id: String::from(*(fields.get(2).expect("error parsing ref_id"))),
+                SAMfields: fields.into_iter().map(|data| {String::from(data)}).collect::<Vec<String>>()
+            };
+
+            newSamLine.score = Some(find_sam_align_score(&mut newSamLine));
+
+            return Some(newSamLine);
+        }    
+    }
+
+
+    fn find_sam_align_score(data: &SamLine) -> f32
+    {
+        let readLength: f32 = data.read_length as f32;
+
+        for field in data.SAMfields.clone()
+        {
+            if field.starts_with("AS:i:")
+            {
+                return (field[5..].parse::<i32>().expect("unable to parse field as i32 in find_sam_align_score") as f32) + (data.read_length as f32);
+            }
         }
 
-        let mut newSamLine = SamLine
-        { 
-            line: newLine,
-
-            fields: None,
-
-            read_id: None, 
-            read_length: None, 
-            position: None, 
-            score: None, 
-            btwsFlg: None, 
-            unmapped: None, 
-            ref_id: None
-        };
-
-        newSamLine.fields = Some(newSamLine.line.split("\t").map(|s| {String::from(s)}).collect());
-
-        newSamLine.read_id = Some(String::from(newSamLine.fields.as_ref().unwrap()[0].as_str()));
-        newSamLine.read_length = Some(newSamLine.fields.as_ref().unwrap()[9].len() as u32);
-        newSamLine.position = Some(newSamLine.fields.as_ref().unwrap()[3].parse::<u32>().unwrap());
-        newSamLine.btwsFlg = Some(newSamLine.fields.as_ref().unwrap()[1].parse::<u32>().unwrap());
-        newSamLine.unmapped = Some(newSamLine.btwsFlg.as_ref().unwrap() & 4 == 4);
-        newSamLine.ref_id = Some(String::from(newSamLine.fields.as_ref().unwrap()[2].as_str()));
-
-        newSamLine.score = None; //implement later!
+        panic!("unable to find sam alignment score!")
 
 
-        return Some(newSamLine);
-    }    
-}
+    }
 
 
-fn find_sam_align_score(fields: Option<Vec<String>>)
-{
-    unimplemented!()
-}
-
-
-enum parseResult<T>
-{
-    Ok(T),
-    Ignore,
-    EOF,
-    Err(String)
-}
-
-///parses one line of a .SAM file, taking reference to a BufReader<File>
-/// 
-///returns Some(SamLine) if data was read or None if none was read
-fn parseSam(SAMReader: &mut BufReader<File>) -> parseResult<SamLine>
-{
-    let mut buf: String = String::new();
-    match SAMReader.read_line(&mut buf)
+    pub enum parseResult<T>
     {
-        Ok(code) => 
+        Ok(T),
+        Ignore,
+        EOF,
+        Err(String)
+    }
+
+    ///parses one line of a .SAM file, taking reference to a BufReader<File>
+    /// 
+    ///returns Some(SamLine) if data was read or None if none was read
+    pub fn parseSAM(SAMReader: &mut BufReader<File>, pScoreCutoff: Option<f32>) -> parseResult<SamLine>
+    {
+        let pScoreCutoff: f32 = pScoreCutoff.unwrap_or(0.01);
+
+        let mut buf: String = String::new();
+        match SAMReader.read_line(&mut buf)
         {
-            if code == 0
+            Ok(code) => 
             {
-                return parseResult::EOF
-            }
-            else
-            {
-                match SamLine::new(String::from(buf.trim()))
+                if code == 0
                 {
-                    None => return parseResult::Ignore,
-                    Some(newLine) => return parseResult::Ok(newLine)
+                    return parseResult::EOF
                 }
+                else
+                {
+                    match SamLine::new(String::from(buf.trim()))
+                    {
+                        None => return parseResult::Ignore,
+                        Some(newLine) =>
+                        {
+                            if newLine.score.expect("error unwrapping newline.score in parseSAM") > pScoreCutoff
+                            {
+                                return parseResult::Ok(newLine); 
+                            }
+                            else
+                            {
+                                return parseResult::Ignore;
+                            }
+                            
+                        }
+                    }
+                }
+                
             }
-            
-        }
-        Err(_) => return parseResult::Err(String::from("Error propagated in parseSam from SamLine::new"))
-    }   
+            Err(_) => return parseResult::Err(String::from("Error propagated in parseSAM from SamLine::new"))
+        }   
+    }
 }
 
 
 #[cfg(test)]
 mod tests {
+    use crate::SAMParser::*;
+    use std::io::BufReader;
+    use std::fs::File;
     use super::*;
 
-    ///tests the parseSam function
+
     #[test]
-    fn testParseSam() {
+    fn testBuildMatrix()
+    {
+
+    }
+
+
+
+    ///tests the parseSAM function
+    #[test]
+    fn testParseSAM()
+    {
         let SAMFile = File::open("TestFiles/test_al.sam").expect("Invalid file");
         let mut SAMReader = BufReader::new(SAMFile);
         loop
         {
-            let newLine = super::parseSam(&mut SAMReader);
+            let newLine = parseSAM(&mut SAMReader, None);
             match newLine
             {
-                parseResult::Ok(data) => println!("{:?}", data.fields),
+                parseResult::Ok(data) => println!("{:?}", data),
                 parseResult::EOF => return,
                 parseResult::Err(msg) => return,
                 parseResult::Ignore => continue
