7ad147038a583bdbc92100dca345c670f51c9325
SnickerDoodlePop <markus@swoveland.com>
2023-01-09 16:05:19 -0800

    The parser is one step away from being a minimum viable project; the next step is to include the Score function.

--- utils/expectation_maximization/src/lib.rs
+++ utils/expectation_maximization/src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(nonstandard_style)]
+#![allow(unused)]
 
 use std::{io::{BufReader, BufRead}, fs::File};
 
@@ -21,16 +22,17 @@ impl SamLine
 {
     ///Create a new SamLine object by consuming a String object
     ///
-    ///Returns none if the provided String is empty
+    ///Returns none if the provided String is to be ignored
     pub fn new(newLine: String) -> Option<SamLine>
     {
-        if newLine.is_empty()
+        if newLine.is_empty() || newLine.starts_with("#") || newLine.starts_with("@")
         {
             return None;
         }
+
         let mut newSamLine = SamLine
         { 
-            line: newLine, 
+            line: newLine,
 
             fields: None,
 
@@ -42,40 +44,63 @@ impl SamLine
             unmapped: None, 
             ref_id: None
         };
-        //parse line into fields
+
         newSamLine.fields = Some(newSamLine.line.split("\t").map(|s| {String::from(s)}).collect());
 
+        newSamLine.read_id = Some(String::from(newSamLine.fields.as_ref().unwrap()[0].as_str()));
+        newSamLine.read_length = Some(newSamLine.fields.as_ref().unwrap()[9].len() as u32);
+        newSamLine.position = Some(newSamLine.fields.as_ref().unwrap()[3].parse::<u32>().unwrap());
+        newSamLine.btwsFlg = Some(newSamLine.fields.as_ref().unwrap()[1].parse::<u32>().unwrap());
+        newSamLine.unmapped = Some(newSamLine.btwsFlg.as_ref().unwrap() & 4 == 4);
+        newSamLine.ref_id = Some(String::from(newSamLine.fields.as_ref().unwrap()[2].as_str()));
 
-/*To do here..:
-    update the constructor of the samline class so its not dumb like it is rn
-    set the rest of the class attributes
-*/
-
-
-
-
-
+        newSamLine.score = None; //implement later!
 
 
         return Some(newSamLine);
     }    
-
 }
 
 
+fn find_sam_align_score(fields: Option<Vec<String>>)
+{
+    unimplemented!()
+}
 
 
+enum parseResult<T>
+{
+    Ok(T),
+    Ignore,
+    EOF,
+    Err(String)
+}
 
 ///parses one line of a .SAM file, taking reference to a BufReader<File>
 /// 
 ///returns Some(SamLine) if data was read or None if none was read
-fn parseSam(SAMReader: &mut BufReader<File>) -> Option<SamLine>
+fn parseSam(SAMReader: &mut BufReader<File>) -> parseResult<SamLine>
 {
     let mut buf: String = String::new();
     match SAMReader.read_line(&mut buf)
     {
-        Ok(_) => return SamLine::new(String::from(buf.trim())),
-        Err(_) => return None
+        Ok(code) => 
+        {
+            if code == 0
+            {
+                return parseResult::EOF
+            }
+            else
+            {
+                match SamLine::new(String::from(buf.trim()))
+                {
+                    None => return parseResult::Ignore,
+                    Some(newLine) => return parseResult::Ok(newLine)
+                }
+            }
+            
+        }
+        Err(_) => return parseResult::Err(String::from("Error propagated in parseSam from SamLine::new"))
     }   
 }
 
@@ -87,15 +112,17 @@ mod tests {
     ///tests the parseSam function
     #[test]
     fn testParseSam() {
-        let SAMFile = File::open("TestFiles/to_isolates.sam").expect("Invalid file");
+        let SAMFile = File::open("TestFiles/test_al.sam").expect("Invalid file");
         let mut SAMReader = BufReader::new(SAMFile);
         loop
         {
             let newLine = super::parseSam(&mut SAMReader);
             match newLine
             {
-                Some(data) => println!("{:?}", data.fields),
-                None => return
+                parseResult::Ok(data) => println!("{:?}", data.fields),
+                parseResult::EOF => return,
+                parseResult::Err(msg) => return,
+                parseResult::Ignore => continue
             }
         }
     }
